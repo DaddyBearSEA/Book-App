2c71e36576bc9b6e12f7b51fcf505db9e046544a
NickDorkins <nick.dorkins@gmail.com>
2020-10-31 15:28:26 -0700

    Trello task 1 complete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Bring in Dependencies
 
 const express = require('express');
-const superagen = require('superagent');
+const superagent = require('superagent');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -20,8 +20,19 @@ app.use(cors());
 // Server looks for pages to serve browser
 app.use(express.static('./public'));
 
+// Set default view engine
+app.set('view engine', 'ejs');
 
+// Routes
+app.get('/', (request, response) => {
+  console.log('No Booooooooleans here!')
+response.status(200).render('pages/index');
+});
 
+app.get('/hello', (request, response) => {
+console.log('I\'ll wear your face as a mask');
+response.status(200).render('pages/hello');
+});
 
 
 
