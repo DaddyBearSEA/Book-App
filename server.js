28da29d920b5f655b5bc62eb7dd2406bcb6d4ec7
NickDorkins <nick.dorkins@gmail.com>
2020-11-08 15:06:37 -0800

    rendering things

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ function Spookybooks(obj) {
   this.authors = (obj.authors) ? obj.authors: 'This was ghost written, no author found';
   this.title = (obj.title) ? obj.title: 'Who says a book needs a title? No title found.';
   this.description = (obj.description) ? obj.description: 'Oh, you want to know what this book is about? Read it, you\'ll figure it out.';
-  this.image_url = (obj.imageLinks.thumbnail) ? obj.imageLinks.thumbnail: "https://i.imgur.com/J5LVHEL.jpg";
+  this.image_url = (obj.imageLinks.smallThumbnail) ? obj.imageLinks.smallThumbnail: "https://i.imgur.com/J5LVHEL.jpg";
   // console.log('Constructor HIT!', this.authors);
 }
 
